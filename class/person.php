c7861d08c22909c927c3089092fab244297c5911
dcneuts <dcneuts@gmail.com>
2017-04-29 11:43:32 -0700

    Created new PHP file for demonstration of PHP classes, have not yet put the right code into index.php

---
+++ class/person.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Derek
+ * Date: 4/29/2017
+ * Time: 10:59 AM
+ * PHP Class Demo
+ */
+
+class Person {
+    protected $name = "Julian";
+    protected $dateOfBirth;
+    public function getName(){
+        return $this->name;
+    }
+
+}
+
+$person = new Person();
+echo $person->getName();

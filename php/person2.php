be9c79ab4933a946d6bda01cba1263baa21b646e
dcneuts <dcneuts@gmail.com>
2017-04-30 00:19:14 -0700

    Creating new demo for class exercise

--- php/person2.php
+++ php/person2.php
@@ -15,18 +15,10 @@ class Person {
         $this->name = $name;
     }
     public function __get($prop) {
-        return $this->$prop." is in the array.<br>";
+        return $this->$prop;
     }
-    //With set, first is the origin and then what you'd like it to be
     public function __set($prop,$value) {
         $this->$prop = $value;
     }
 }
-$names = array("Benjamin","Rafa","Hassan","Jade","Ali","Rob");
-
-for ($i=0;$i<sizeof($names);$i++) {
-    $person = new Person($names[$i]);
-    //Override the names in the array and echo confirmation of same
-    $person->name = "Test";
-    echo $person->name;
-}
+$person = new Person("Ben","1987-2-19");

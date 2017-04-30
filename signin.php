951f883f59fac32d189a5962f26d0fc5ebc10943
dcneuts <dcneuts@gmail.com>
2017-04-30 01:48:19 -0700

    Getting Shawpify site set back up after long break, creating template for sign-in page and will be dynamically created

---
+++ signin.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Shaw Academy Practical Application Project">
+    <meta name="author" content="Derek Neuts">
+    <title>Shawpify Home</title>
+    <!-- Bootstrap CSS Components from CDN -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<!--Comments will be included throughout for instructional review-->
+<body>
+<!--Body Header-->
+<header>
+    <div class='container-fluid'>
+        <div class='jumbotron text-center'>
+            <h1>Shawpify</h1>
+            <p>
+                Offering a wide range of products from consumer electronics to cars</p>
+        </div>
+    </div>
+</header>
+<!--Nav Section-->
+<nav class='navbar navbar-inverse navbar-fixed-top'>
+    <div class='container-fluid'>
+        <!--Body Header-->
+        <header class='navbar-header'>
+            <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>
+                <span class='glyphicon glyphicon-menu-hamburger mobmenu'></span>
+            </button>
+            <a class='navbar-brand' href='index.html'>Shawpify Home</a>
+        </header>
+        <div class='collapse navbar-collapse' id='myNavbar'>
+            <ul class='nav navbar-nav'>
+                <li>
+                    <a href='#'>Products</a>
+                </li>
+                <li>
+                    <a href='#'>Deals</a>
+                </li>
+            </ul>
+            <ul class='nav navbar-nav navbar-right'>
+                <li>
+                    <a href='signin.php'><span class='glyphicon glyphicon-user'>Sign In</span></a>
+                </li>
+                <li>
+                    <a href='#'><span class='glyphicon glyphicon-shopping-cart'>Cart</span></a>
+                </li>
+                <li>
+                    <form class='navbar-form' action='#' method='POST'>
+                        <input type='text' class='form-control' placeholder='Search for a Product'>
+                        <button type='submit' class='btn btn-primary'>
+                            <span class='glyphicon glyphicon-search'></span>
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!--Main-->
+<main class="container">
+
+</main>
+<!--Footer Area-->
+<footer class='text-center'>
+    <nav class='navbar navbar-inverse navbar-fixed-bottom'>
+        <div class='container-fluid'>
+            <ul class='nav navbar-nav'>
+                <li class='navbar-text'>&copy; Shaw Academy</li>
+                <li>
+                    <a href='#'><i class='fa fa-facebook'></i></a>
+                </li>
+                <li>
+                    <a href='#'><i class='fa fa-google-plus'></i></a>
+                </li>
+                <li>
+                    <a href='#'><i class='fa fa-twitter'></i></a>
+                </li>
+            </ul>
+            <ul class='nav navbar-nav navbar-right'>
+                <li>
+                    <a href='#'><span>Site Map</span></a>
+                </li>
+                <li>
+                    <a href='#'><span>Contact Us</span></a>
+                </li>
+                <li>
+                    <a href='#'><span>Terms &amp; Conditions</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</footer>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+<!-- Custom JS -->
+
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Derek
+ * Date: 4/30/2017
+ * Time: 1:38 AM
+ */

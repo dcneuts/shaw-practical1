d73287ab35f811cc5f5eae92e9b80e553252fe36
dcneuts <dcneuts@gmail.com>
2017-05-01 23:23:55 -0700

    Created HTML form for signup and made a link to new file login.php which has not yet been created

--- signin.php
+++ signin.php
@@ -11,7 +11,20 @@
 
     <!--Main-->
     <main class="container">
-
+        <form action="inc/login.php" method="post">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input id="username" type="text" class="form-control" name="username">
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input id="password" type="password" class="form-control" name="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+        <hr>
+        <p>Don&#8217;t have an account?</p>
+        <a href="signup.php" class="btn btn-primary">Sign Up Here</a>
     </main>
 
 

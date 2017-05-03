0a43e988f31c94ca9cdb56400b9307ec9129329f
dcneuts <dcneuts@gmail.com>
2017-05-02 23:17:41 -0700

    First attempt at creating login and configuration file for database access with Bootstrap page

--- inc/login.php
+++ inc/login.php
@@ -3,7 +3,14 @@
     //$_POST['username'];
     //$_POST['password'];
 
-    if(isset($_POST['username'])||!isset($_POST['password'])) {
+    if(!isset($_POST['username'])||!isset($_POST['password'])) {
         //below for debugging
         die("Error:".var_dump($_POST));
+    }
+
+    include_once "config.php";
+    $conn = new mysqli(SERVER_NAME,USERNAME,PASSWORD,DATABASE);
+
+    if($conn->connect_error){
+        die("Connection failed:".$conn->connect_error);
     }

f57924b41fdd87f83a033b21f8ff9bc6f0f78594
dcneuts <dcneuts@gmail.com>
2017-05-07 12:38:26 -0700

    Created template file, will be filling in details to pull data

---
+++ templates/template.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Derek
+ * Date: 5/7/2017
+ * Time: 12:04 PM
+ */
+class Template {
+    private $template;
+    private $content;
+
+    public function __construct($template,$content) {
+        // Should validate arguments before continuing
+        $this->load($template,$content);
+    }
+
+    public function output(){
+
+}
+
+    public function load($template,$content) {
+        $this->template = file_get_contents("http://localhost/shawpractical/templates/$template",true);
+        //$this->template = $template;
+        $this->content = $content;
+    }
+}

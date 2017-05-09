1cb5ce6e449621ba180a3323926c9ac2d41e5f69
dcneuts <dcneuts@gmail.com>
2017-05-08 21:17:46 -0700

    Building out the create account process and learning how to hash and salt password based on algo selected

--- inc/create_account.php
+++ inc/create_account.php
@@ -15,3 +15,25 @@ $_POST['password']
 if(!isset($_POST['username'])||!isset($_POST['email'])||!isset($_POST['password'])){
     die("Data Error");
 }
+
+//assume validity of data
+include_once "config.php";
+
+$conn = new mysqli(SERVER_NAME, USERNAME, PASSWORD, DATABASE);
+if($conn->connect_error){
+    die("Connection failed: ".$conn->connect_error);
+}
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+//salt and hash
+$password = password_hash($_POST['password'],PASSWORD_BCRYPT);
+
+$stmt = $conn->prepare("INSERT INTO `users` (`username`,`email`,`password`) VALUES (?,?,?)");
+
+$stmt->bind_param("sss",$username,$email,$password);
+
+//execute
+$stmt->execute();
+
+$conn->close();

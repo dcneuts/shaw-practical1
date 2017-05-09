e42313efbca90d21f7a78bece145f520a4ef6071
dcneuts <dcneuts@gmail.com>
2017-05-08 19:59:49 -0700

    Created account sign-up page, will now need to create PHP to control this function

--- signup.php
+++ signup.php
@@ -5,3 +5,37 @@
  * Date: 5/1/2017
  * Time: 11:24 PM
  */
+    include "inc/head.inc.php";
+?>
+    <!--Header-->
+    <header>
+        <div class='container'>
+            <h1>Create an Account</h1>
+        </div>
+    </header>
+    <body>
+
+    <!--Main-->
+    <main class="container">
+        <form action="inc/create_account.php" method="post">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input id="username" type="text" class="form-control" name="username">
+            </div>
+            <div class="form-group">
+                <label for="username">Email:</label>
+                <input id="username" type="text" class="form-control" name="email">
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input id="password" type="password" class="form-control" name="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Create Account</button>
+        </form>
+    </main>
+
+
+    <!--Footer Area-->
+<?php
+    include "inc/foot.inc.php";
+?>

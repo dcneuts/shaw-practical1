ca2cb7ae3595cd4a537fc292a7636e1339c7e006
Derek Neuts <dcneuts@gmail.com>
2017-05-15 23:19:41 -0700

    Added a dynamic image loading JS file that will be integrated into product carousel

---
+++ js/dynamicimageloading.js
@@ -0,0 +1,25 @@
+/**
+ * Created by derek on 5/15/17.
+ */
+
+$(document).ready(function(){
+
+	$('.caption>h4>a').on('click',function(event){
+		event.preventDefault();
+
+		$this = $(this);
+
+		//getData = $this.html().replace(" ","+");
+
+		$.getJSON($this.attr('href'),{product:$this.html()},function(json){
+			$('#carousel').fadeOut(200, function(){
+				$carouselImages = $('#carousel .slide-image');
+				for(i=0;i<3;i++){
+					$($carouselImages[i]).attr("src",json[i]);
+				}
+				$('#carousel').fadeIn(200);
+			});
+		});
+	});
+
+});

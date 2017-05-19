f9122c1d1c46291c9a2c58db30dc7cd34bd1ef69
Derek Neuts <dcneuts@gmail.com>
2017-05-19 01:07:19 -0700

    Preliminary Ajax config

--- js/ajax.js
+++ js/ajax.js
@@ -3,7 +3,14 @@
  */
 $(document).ready(function () {
 	$("body>nav a").on("click",function (event) {
+		this
 		event.preventDefault();
-		$("main")
-	})
+		$("main").load($(this).attr("href")+" main",completeFunction);
+	});
+//Errors will get printed to contents of Main
+	function completeFunction(responseText, textStatus, request) {
+		if(textStatus == "error"){
+			$("main").text("An error has occurred: "+ request.status+" "+request.statusText);
+		}
+	}
 });

c582698a1a39d0162926f715173dc2513cb61c19
Derek Neuts <dcneuts@gmail.com>
2017-05-20 12:28:14 -0700

    Override of default behaviors of form, setting up loadSearch function

--- js/ajax.js
+++ js/ajax.js
@@ -14,7 +14,21 @@ $(document).ready(function () {
 	function loadMain($element) {
 		$("main").load($element.attr("href")+" main",completeFunction);
 	}
-	
+
+	//ajax search bar functionality to override default html behaviors
+	//instructors note that in real-world scenario, class or id would be targeted, not generic
+	$("body>nav form").on("click", function (event) {
+		//prevent default behaviors
+		event.preventDefault();
+		//makes permanent reference to form once clicked
+		$target = $(this);
+		$("main").fadeOut(300,function () {
+			loadSearch($target);
+		});
+	});
+	function loadSearch($element){
+
+	}
 //Errors will get printed to contents of Main
 	function completeFunction(responseText, textStatus, request) {
 		if(textStatus == "error"){

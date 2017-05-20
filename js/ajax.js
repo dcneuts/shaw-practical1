b8843b612f2b8da118cf2f3620eaaf865e760837
Derek Neuts <dcneuts@gmail.com>
2017-05-20 12:06:02 -0700

    Completed functions to make screen fade in and out, just for testing purposes

--- js/ajax.js
+++ js/ajax.js
@@ -1,21 +1,26 @@
-/**
- * Created by derek on 5/19/17.
- */
 $(document).ready(function () {
+	//ajax navbar anchors
 	$("body>nav a").on("click",function (event) {
+		//caches the function with reference to anchor tag
+		$target = $(this);
 		event.preventDefault();
 		//Number is in milliseconds
 		//Callback function after time is up
 		$("main").fadeOut(200,function () {
-			$("main").load($(this).attr("href")+" main",completeFunction);
+			//use the cache from above for callback
+			loadMain($target);
 		});
 	});
+	function loadMain($element) {
+		$("main").load($element.attr("href")+" main",completeFunction);
+	}
+	
 //Errors will get printed to contents of Main
 	function completeFunction(responseText, textStatus, request) {
 		if(textStatus == "error"){
 			$("main").text("An error has occurred: "+ request.status+" "+request.statusText);
 		} else {
-			$("main").fadeIn(300);
+			$("main").fadeIn(200);
 		}
 	}
 });
